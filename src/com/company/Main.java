a77bb588d4bb290249eb8e94b82c4952dfffa6f5
Dylan Morrison <dylanmorrison30@yahoo.com>
2021-08-19 12:40:28 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	System.println("Ok this is an applicaiton I guess");
+	int rem = 5;
+	for(int i = 0; i <= rem; i++){
+	    System.println("lets go " + i);
+    }
+    }
+}
